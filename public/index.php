00f74098f98fa4146acd34fb1d9ca4c0d3a313c7
Kolesnikov Dmitry - Slash <dmi56756877@mail.ru>
2019-11-26 17:30:13 +0900

    commit

--- public/index.php
+++ public/index.php
@@ -4,11 +4,13 @@ $query = rtrim($_SERVER['QUERY_STRING'], "/");
 require '../vendor/core/Router.php';
 require '../vendor/libs/function.php';
 
-$router = new Router();
 
-Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
-Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
-Router::add('', ['controller' => 'Main', 'action' => 'index']);
+// Router::add('posts/add', ['controller' => 'Posts', 'action' => 'add']);
+// Router::add('posts', ['controller' => 'Posts', 'action' => 'index']);
+// Router::add('', ['controller' => 'Main', 'action' => 'index']);
+
+Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
+Router::add('([a-z-]+)/([a-z-]+)');
 
 debug(Router::getRoutes());
 
--- vendor/core/Router.php
+++ vendor/core/Router.php
@@ -1,28 +1,48 @@
 <?php
 class Router 
 {
-    // public function __construct() 
-    // {
-    //     echo "Привет, мир";
-    // }
+    /*
+        Таблица маршрутов
+        @var $array
+    */
     protected static $routes = [];
+    
+    /*
+        Текущий маршрутов
+        $var $array
+    */
     protected static $route = [];
 
+    /*
+        добавляет маршрут в таблицу маршрутов
+        
+        @param string $regexp регулярное выражение маршрута
+        @param $array $route маршрут ([controller, action, params])
+    */
     public static function add($regexp, $route = []) {
         self::$routes[$regexp] = $route;
     }
-
+    /*
+        возвращает таблицу маршрутов 
+        @return array
+    */
     public static function getRoutes() {
         return self::$routes;
     }
-
+    /*
+        ищет URL в таблице маршрутов
+        @param string $url входящий URL
+        @return boolen
+    
+    */
     public static function getRoute() {
         return self::$route;
     }
     
     public static function matchRoute($url) {
         foreach (self::$routes as $pattern => $route) {
-            if ($url == $pattern) {
+            if (preg_match("#$pattern#i", $url, $matches)) {
+                debug($matches);
                 self::$route = $route;
                 return true;
             }
