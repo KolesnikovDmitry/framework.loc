84ee12ba47b80e16fa8b07fefeb6c58ddcc52d76
Slash <dmi56756877@yandex.ru>
2021-02-27 17:17:07 +0900

    change name proqect

--- config/init.php
+++ config/init.php
@@ -27,5 +27,20 @@ define("CONF", ROOT . '/config');
 //шаблон нашего сайта по умолчанию
 define("LAYOUT", 'default');
 
+//http`://framework.loc/public/index.php
+$app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
 
-echo $app_path = "http://{$_SERVER['HTTP_HOST']}{$_SERVER['PHP_SELF']}";
+//по шаблону вырежем из адресной строки index.php. Ищем любые символы, все кроме слеша, начиная с конца строки,
+//замещая пустой строкой в переменной app_path
+//"http`://framework.loc/public/" 
+$app_path = preg_replace("#[^/]+$#", ' ', $app_path);
+
+//вырежем public и получим url главной страницы
+//http`://framework.loc/
+$app_path = str_replace('/public/', ' ', $app_path);
+
+//запишем этот url в константу
+define("PATH", $app_path);
+
+
+
