86514edd1c12af6b976ab7f71116f3ab256591d1
Dmitry Kolesnikov <dmi56756877@yandex.ru>
2023-01-19 12:56:52 +0900

    reg routes

--- public/index.php
+++ public/index.php
@@ -1,12 +1,18 @@
 <?php
-$query = $_SERVER['QUERY_STRING'];
+$query = rtrim($_SERVER['QUERY_STRING'], '/') ;
 
 require_once "../vendor/libs/function.php";
 require_once '../vendor/core/Router.php';
 
-Router::add('posts/add', ['controller'=>'Posts', 'action'=>'add']);
-Router::add('posts/', ['controller'=>'Posts', 'action'=>'index']);
-Router::add('', ['controller'=>'Main', 'action'=>'index']);
+// Router::add('posts/add', ['controller'=>'Posts', 'action'=>'add']);
+// Router::add('posts', ['controller'=>'Posts', 'action'=>'index']);
+// Router::add('', ['controller'=>'Main', 'action'=>'index']);
+
+Router::add('^$', ['controller'=>'Main', 'action'=>'index']);
+Router::add('([a-z-]+)/([a-z-]+)');
+
+
+debug(Router::getRoutes());
 
 if(Router::matchRoute($query)) {
     debug(Router::getRoute());
