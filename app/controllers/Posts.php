f3581b37b28bbbd043e9119c130362b6a2fa0c6a
Dmitry Kolesnikov <dmi56756877@yandex.ru>
2023-01-31 12:35:19 +0900

    add base controller

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -1,10 +1,19 @@
 <?php
 
 namespace app\controllers;
+use vendor\fw\core\base\Controller;
 
-class Posts {
-    public function IndexAction()
+class Posts extends Controller{
+    
+ 
+    public function indexAction()
     {
         echo "Posts::Index";
+    }
+
+    public function testAction()
+    {
+        debug($this->route);
+        echo "Posts::test";
     }
 }
